d6df62232d678e10470ac5cf06e4715a3094f630
Olivia <mannerolivia@gmail.com>
2024-12-10 12:24:00 +0100

    Creation of text for displaying Home, Map, Profile, Search screens, Update package.json with expo gradient, Addition cors in app.js, Update all related-fetch code in SignIn screen

---
+++ screens/ProfileScreen.js
@@ -0,0 +1,15 @@
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+
+  return (
+    <View>
+        <Text>Profile Screen</Text>
+    </View>
+  )
+    
+}
+
+const styles = StyleSheet.create({
+  
+});
