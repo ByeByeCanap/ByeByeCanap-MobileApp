9f68f2f2e3dc3f932882c10fc8fd9dc1a00e70b9
Thomas <thmas.garcia@gmail.com>
2024-12-12 11:24:57 +0100

    Create ProfilScreen + Updated HomeScreen

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,15 +1,86 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { LinearGradient } from "expo-linear-gradient";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-export default function HomeScreen() {
+export default function ProfilScreen( { navigation } ) {
+
+  const GoBack = () => navigation.navigate("TabNavigation");
 
   return (
-    <View>
-        <Text>Profile Screen</Text>
+    <View style={styles.container}>
+          <LinearGradient
+            style={styles.header}
+            colors={["#fdc731", "#f3773b"]}
+            useAngle={true}
+            angle={135}
+            start={{ x: 0, y: 1 }}
+          >
+            <Image
+              style={styles.logoIcon}
+              resizeMode="cover"
+              source={require("../assets/logoIcon.png")}
+            />
+          </LinearGradient>
+          <FontAwesome name="arrow-left" size={30} onPress={GoBack} />
+
+          <View style={styles.textContainer}>
+            <Text style={styles.text}>Personnaliser</Text>
+            <FontAwesome name="pencil" size={30} />
+          
+            <Text style={styles.text}>Thème clair/sombre</Text>
+            <FontAwesome name="moon-o" size={30} />
+
+            <Text style={styles.text}>Paramètres</Text>
+            <FontAwesome name="gear" size={30} />
+
+            <Text style={styles.text}>Compte</Text>
+            <FontAwesome name="user" size={30} />
+
+            <Text style={styles.text}>Aide</Text>
+            <FontAwesome name="question" size={30} />
+
+            <Text style={styles.text}>Se déconnecter</Text>
+            <FontAwesome name="sign-out" size={30} />
+          </View>
+
+          <LinearGradient
+                  style={styles.footer}
+                  colors={["#fdc731", "#f3773b"]}
+                  start={{ x: 0, y: 1 }}
+          />
     </View>
   )
     
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#ffffff",
+  },
+  header: {
+    width: "100%",
+    height: 120,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+    paddingTop: 30,
+    backgroundColor: "transparent",
+  },
+  logoIcon: {
+    width: 50,
+    height: 50,
+  },
+  headerIcons: {
+    flexDirection: "row",
+    gap: 20,
+  },
+  text: {
+    fontWeight: "600",
+    fontSize: 26,
+  },
+  textContainer: {
+    
+  }
 });
