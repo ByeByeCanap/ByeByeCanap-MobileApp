022647523739a391f483f1267e9256d4dc76eb33
Olivia <mannerolivia@gmail.com>
2024-12-19 13:08:06 +0100

    Update SearchEventScreen with correction of fetch to send data including eventId to the next screen (EventDetailScreen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,13 +1,13 @@
 import React, { useEffect } from "react";
 import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  ImageBackground,
-  ScrollView,
-  Image,
-  Pressable,
+    StyleSheet,
+    View,
+    Text,
+    TouchableOpacity,
+    ImageBackground,
+    ScrollView,
+    Image,
+    Pressable,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
@@ -18,202 +18,212 @@ import { Header } from "../components/header";
 // Import for fetch
 import { BACK_IP } from "@env";
 
-export default function SearchScreen({ navigation , route}) {
+export default function SearchScreen({ navigation, route }) {
 
-      const token = route.params.token;
-      const id = route.params.id;
-      const categoryName = route.params.categoryName;
-      //console.log("look user.token", token, "look data.id", id, "look at category:", categoryName);
-         
-     // Hook to update the list of themes and categories
-      const [eventByCategory, setEventByCategory] = useState([]);
+  // Retreive information from previous page
+    const token = route.params.token;
+    const id = route.params.id;
+    const categoryName = route.params.categoryName;
+    //console.log("look user.token", token, "look data.id", id, "look at category:", categoryName);
 
-      useEffect(() => {
+    // Hook to update the list of themes and categories
+    const [eventByCategory, setEventByCategory] = useState([]);
+
+    useEffect(() => {
         const fetchEventsByCategory = async () => {
-          try {
-            const response = await fetch(`${BACK_IP}/events/byCategory/${categoryName}`);
-            const data = await response.json();
-           // console.log("Data fetch events by category", data);
-            setEventByCategory(data);
-          } catch (error) {
-            console.error("Error fetching events by category:", error.message);
-          }
-        };
-      
-        fetchEventsByCategory();
-      }, [categoryName]);
+            try {
+                const response = await fetch(
+                    `${BACK_IP}/events/byCategory/${categoryName}`
+                );
+                const events = await response.json();
 
-      function gotToDetails() {
-        //console.log("Ou est la CATEGORYYYYY", data.categorie[0], "DATA:",data);
+                setEventByCategory(events);
+            } catch (error) {
+                console.error(
+                    "Error fetching events by category:",
+                    error.message
+                );
+            }
+        };
+
+        fetchEventsByCategory();
+    }, [categoryName]);
+
+    //console.log("Events", eventByCategory);
+    const eventsIds = eventByCategory.map((event, index) => event._id);
+    console.log("Data fetch events by category", eventsIds);
+
+    function gotToDetails(eventsIds) {
+        console.log("DATA:", eventsIds);
         navigation.navigate("EventDetailScreen", {
             token,
-            id,
+            eventsIds,
         });
     }
-      
-      //console.log("look at getter", eventByCategory);
 
-      const card = eventByCategory.map(event => {
+    //console.log("look at getter", eventByCategory);
 
+    const card = eventByCategory.map((event, index) => {
         return (
-          <View style={styles.card}>
-            <View style={styles.imageContainer}>
-              <Pressable onPress={() => gotToDetails()}>
-              <Image source={require('../assets/imagesEvent/event_main.jpg')} style={styles.image} />
-              </Pressable>
+            <View key={index} style={styles.card}>
+                <View style={styles.imageContainer}>
+                    <Pressable onPress={() => gotToDetails(event._id)}>
+                        <Image
+                            source={require("../assets/imagesEvent/event_main.jpg")}
+                            style={styles.image}
+                        />
+                    </Pressable>
+                </View>
+
+                <View style={styles.globalContainer}>
+                    <View>
+                        <Text style={styles.h2}>
+                            {event.eventDate.slice(0, 10)}
+                        </Text>
+                    </View>
+
+                    <Text style={styles.h1Event}>{event.title}</Text>
+                    <Text style={styles.body}>{event.location}</Text>
+                    <Text style={styles.body}>{event.participants.length}</Text>
+                </View>
             </View>
-      
-            <View style={styles.globalContainer}>
-              <View>
-                <Text style={styles.h2}>{event.eventDate.slice(0,10)}</Text>
-              </View>
-      
-              <Text style={styles.h1Event}>{event.title}</Text>
-              <Text style={styles.body}>{event.location}</Text>
-              <Text style={styles.body}>{event.participants.length}</Text>
-            </View>
-          </View>
         );
+    });
 
-      })
+    const goHome = () => {
+        navigation.navigate("TabNavigator");
+    };
 
+    return (
+        <View style={styles.container}>
+            <LinearGradient
+                style={styles.header}
+                colors={["#fdc731", "#f3773b"]}
+                useAngle={true}
+                angle={135}
+                start={{ x: 0, y: 1 }}
+            >
+                <Image
+                    style={styles.logoIcon}
+                    resizeMode="cover"
+                    source={require("../assets/logoIcon.png")}
+                />
+            </LinearGradient>
 
-  
-const goHome = () => {
-  navigation.navigate('TabNavigator')
-}
+            <View style={styles.arrow}>
+                <FontAwesome name="arrow-left" size={30} onPress={goHome} />
+                <Text style={styles.h1}>Liste des événements</Text>
+            </View>
 
-
-  return (
-    <View style={styles.container}>
-      <LinearGradient
-        style={styles.header}
-        colors={["#fdc731", "#f3773b"]}
-        useAngle={true}
-        angle={135}
-        start={{ x: 0, y: 1 }}
-      >
-        <Image
-          style={styles.logoIcon}
-          resizeMode="cover"
-          source={require("../assets/logoIcon.png")}
-        />
-      </LinearGradient>
-
-      <View style={styles.arrow}>
-        <FontAwesome name="arrow-left" size={30} onPress={goHome}/>
-        <Text style={styles.h1}>Liste des événements</Text>
-      </View>
-
-      <ScrollView contentContainerStyle={styles.content}>
-      {card} 
-      </ScrollView>
-    </View>
-  );
+            <ScrollView contentContainerStyle={styles.content}>
+                {card}
+            </ScrollView>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-
-  content: {
-    alignItems: "center",
-    justifyContent: "stretch",
-    paddingVertical: 10,
-  },
-
-  arrow: {
-    paddingRight: 10,
-    paddingLeft: 22,
-    flexDirection: "row",
-    alignItems: "center",
-    alignSelf: "flex-start",
-    gap: 20,
-    paddingTop: 20,
-    paddingBottom: 20,
-  },
-
-  header: {
-    width: "100%",
-    height: 120,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingLeft: 22,
-    backgroundColor: "transparent",
-  },
-
-  h1: {
-    fontSize: 20,
-    fontFamily: "ParkinsansMedium",
-    color: "#000",
-  },
-
-  h1Event: {
-    fontSize: 20,
-    fontFamily: "ParkinsansMedium",
-    color: "#000",
-    flexWrap: 'wrap',
-    width: 200,
-  },
-
-  logoIcon: {
-    top: 20,
-    width: 50,
-    height: 50,
-  },
-
-  card: {
-    flexDirection: "row",
-    gap : 10,
-    width: "80%",
-    height: 200,
-    paddingHorizontal: 10,
-    marginVertical: 10,
-    shadowColor: "rgba(0, 0, 0, 0.25)",
-    shadowOffset: {
-      width: 0,
-      height: 4,
+    container: {
+        flex: 1,
+        backgroundColor: "white",
     },
-    shadowRadius: 8,
-    elevation: 8,
-    shadowOpacity: 1,
-    borderRadius: 15,
-    flex: 1,
-    width: "100%",
-    padding: 10,
-    backgroundColor: "#fff",
-  },
 
-  h2: {
-    fontFamily: 'NotoSansDisplayRegular',
-    fontSize: 16,
-  },
+    content: {
+        alignItems: "center",
+        justifyContent: "stretch",
+        paddingVertical: 10,
+    },
 
-  logoProfil : {
-    width: 60,
-    height: 60,
-  },
+    arrow: {
+        paddingRight: 10,
+        paddingLeft: 22,
+        flexDirection: "row",
+        alignItems: "center",
+        alignSelf: "flex-start",
+        gap: 20,
+        paddingTop: 20,
+        paddingBottom: 20,
+    },
 
-  image: {
-    width: 150,
-    height: 150,
-    borderRadius: 10,
-  },
+    header: {
+        width: "100%",
+        height: 120,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingLeft: 22,
+        backgroundColor: "transparent",
+    },
 
-  imageContainer : {
-    justifyContent: "center",
-  },
+    h1: {
+        fontSize: 20,
+        fontFamily: "ParkinsansMedium",
+        color: "#000",
+    },
 
-  globalContainer : {
-    justifyContent: 'center',
-    alignItems: 'flex-start'
-  },
+    h1Event: {
+        fontSize: 20,
+        fontFamily: "ParkinsansMedium",
+        color: "#000",
+        flexWrap: "wrap",
+        width: 200,
+    },
 
-  body: {
-    fontFamily: 'NotoSansDisplayLight',
-    fontSize: 12,
-  }
+    logoIcon: {
+        top: 20,
+        width: 50,
+        height: 50,
+    },
+
+    card: {
+        flexDirection: "row",
+        gap: 10,
+        width: "80%",
+        height: 200,
+        paddingHorizontal: 10,
+        marginVertical: 10,
+        shadowColor: "rgba(0, 0, 0, 0.25)",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowRadius: 8,
+        elevation: 8,
+        shadowOpacity: 1,
+        borderRadius: 15,
+        flex: 1,
+        width: "100%",
+        padding: 10,
+        backgroundColor: "#fff",
+    },
+
+    h2: {
+        fontFamily: "NotoSansDisplayRegular",
+        fontSize: 16,
+    },
+
+    logoProfil: {
+        width: 60,
+        height: 60,
+    },
+
+    image: {
+        width: 150,
+        height: 150,
+        borderRadius: 10,
+    },
+
+    imageContainer: {
+        justifyContent: "center",
+    },
+
+    globalContainer: {
+        justifyContent: "center",
+        alignItems: "flex-start",
+    },
+
+    body: {
+        fontFamily: "NotoSansDisplayLight",
+        fontSize: 12,
+    },
 });
